9ff87902ab2225cf2dae2abe4573ea7ea4126047
sick <sick.skq@gmail.com>
2021-01-17 22:25:10 -0800

    Support testing built files

--- jest.build.config.js
+++ jest.build.config.js
@@ -1,14 +1,12 @@
+// this config is for testing files in build/
 module.exports = {
   testEnvironment: 'node',
-  roots: ['<rootDir>/src/', '<rootDir>/test/'],
+  rootDir: './build',
+  roots: ['<rootDir>/src/', '<rootDir>/test'],
   // only test files with a suffix of .test or .spec in test folder
   testMatch: ['**/test/**/*.@(spec|test).[jt]s'],
-  testPathIgnorePatterns: ['<rootDir>/build/', '<rootDir>/node_modules/'],
   collectCoverage: true,
   collectCoverageFrom: ['**/*.[tj]s', '!**/test/**'],
   coverageDirectory: '<rootDir>/test/coverage',
-  transform: {
-    '^.+\\.[tj]s$': 'ts-jest',
-  },
   snapshotResolver: '<rootDir>/test/configs/snapshot-resolver.js',
 };
